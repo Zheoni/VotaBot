415f89a0110f426c60aad42297b48c660049bd43
Fran <zheoni@outlook.es>
2018-11-08 00:31:30 +0100

    Added basic bot functions. Basic polls, time based vote counting, parsing messages, etc.

--- index.js
+++ index.js
@@ -6,34 +6,69 @@ const emojis = ["0⃣", "1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", 
 
 client.on('ready', () => {
 	console.log(`Bot logged in as ${client.user.tag}!`);
+	client.user.setActivity(`${config.prefix} help`);
 });
 
 client.on('message', async msg => {
 	if (msg.content.startsWith(config.prefix) && !msg.author.bot) {
-		/*const reactions = await msg.awaitReactions(reaction => {
-			console.log(reaction.emoji);
-			return reaction.name == "nine"
-		}, {time: 10000});*/
-		poll(msg);
+		if (msg.content === config.prefix + " help") help(msg);
+		// else if(msg.content === config.prefix + " invite") invite(msg);
+		else poll(msg);
 	}
 });
 
 client.login(config.token);
 
 async function poll(msg) {
-	let _args = msg.content.slice(config.prefix.length).trim().split('"');
-	//Filer more because of multiple whitespaces
-	let args = _args.filter(phrase => {
-		phrase.trim();
-		return phrase !== "" && phrase !== " ";
-	});
+	console.log("Poll executed in " + msg.guild.name + " by " + msg.author.tag);
+
+	let args = await parseToArgs(msg);
+	let time = 0;
+
+	if(args.length == 0) {
+		msg.reply("Sorry, I cannot create an empty poll");
+		return;
+	}
+
+	//parse the time limit if it exists
+	if(args[0].startsWith("-t=")) {
+		const timeRegex = /\d+/;
+		const unitRegex = /s|m|h|d/i;
+		let _time = args.shift();
+		let unit = 's';
+
+		let match1 = _time.match(timeRegex);
+		if(match1 != null) time = parseInt(match1.shift());
+		else{
+			msg.reply("Wrong time syntax!");
+			return;
+		}
+
+		let match2 = _time.match(unitRegex);
+		if(match2 != null) unit = match2.shift();
+
+		switch(unit) {
+			case 's': time *= 1000;
+			break;
+			case 'm': time *= 60000;
+			break;
+			case 'h': time *= 3600000;
+			break;
+			case 'd': time *= 86400000;
+			break;
+			default: time *= 1000;
+		}
+	}
+
+	console.log(args + " - Time: " + time);
 
 	let question = args.shift();
 
-	let pollmsg = await generatePollMsg(question, args);
+	let pollmsgEmbed = await generatePollMsg(question, args);
 
-	await msg.channel.send(pollmsg)
+	await msg.channel.send({ embed: pollmsgEmbed })
 		.then(async message => {
+			if(time > 0) voting(message, time);
 			for (let i = 0; i < args.length && i < 10; i++) {
 				try {
 					await message.react(emojis[i + 1]);
@@ -41,18 +76,52 @@ async function poll(msg) {
 					console.log(error);
 				}
 			}
+			return message;
 		}).catch(console.error);
-	
-	
+}
 
-	console.log(args);
+function parseToArgs(msg) {
+	let args = msg.content.slice(config.prefix.length)
+		.trim()
+		.split('"')
+		.filter(phrase => phrase.trim() != "");
+	for (let i = 0; i < args.length; i++) args[i] = args[i].trim();
+	return args;
 }
 
 function generatePollMsg(question, answers) {
-	let msg;
-	msg = "**" + question + "** " + "\n";
+	let msg = new String();
 	for (let i = 0; i < answers.length && i < 10; i++) {
-		msg += emojis[i+1] + ". " + answers[i] + "\n";
+		msg += emojis[i + 1] + ". " + answers[i] + "\n";
 	}
-	return msg;
+
+	let embed = new Discord.RichEmbed()
+		.setAuthor(question)
+		.setDescription(msg)
+		.setFooter('React with the emojis below');
+
+	return embed;
+}
+
+async function voting(pollmsg, time) {
+	let filter = (reaction) => emojis.includes(reaction.emoji.name);
+	const reactions = await pollmsg.awaitReactions(filter, {time: time});
+	let results = new Array(reactions.size);
+	for(let i = 0; i < reactions.size; i++) {
+		results[i] = reactions.get(emojis[i+1]).count - 1;
+	}
+	return results;
+}
+
+function help(msg) {
+	console.log("Help executed in " + msg.guild.name + " by " + msg.author.tag);
+
+	const helpEmbed = new Discord.RichEmbed()
+		.setAuthor("VotaBot's Commands")
+		.addField("Create Y/N poll", "`" + config.prefix + " \"{question}\"" + "`")
+		.addField("Create complex poll [2-10]", "`" + config.prefix + " \"{question}\" \"[Option 1]\" \"[Option 2]\"..." + "`")
+		.addField("Timed polls", "Just add `-t=TIME[s|m|h|d]` after the \"" + config.prefix + "\", where \"TIME\" is " +
+			"the time to finish the poll followed by it's unit.");
+
+	msg.channel.send({ embed: helpEmbed });
 }
